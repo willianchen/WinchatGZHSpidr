7c25237e1cc652bc22fdf3fb66c1f7767a510aae
cml <willian@126.com>
2019-01-18 17:40:06 +0800

    Attribute

---
+++ Spider.Data/Attribute/TableNameAttrbute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spider.Data.Attribute
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct)]
+    public class TableNameAttribute : System.Attribute
+    {
+        public string tableName;
+
+        public TableNameAttribute(string _tableName)
+        {
+            this.tableName = _tableName;
+        }
+   
+    }
+}
